ada5ad3cf2241b9e51fef1eb28bd923f632aa1f2
SinaBolouki <sina.bolouki@gmail.com>
2021-10-17 23:26:52 +0330

    fix a bug in mac that buttons were white in dark mode

--- interface.py
+++ interface.py
@@ -33,71 +33,71 @@ expression_field = Entry(gui, textvariable=equation)
 expression_field.grid(columnspan=4, ipadx=70)
 
 button1 = Button(gui, text=' 1 ', fg='black', bg='red', height=1, width=7
-                 , command=lambda: press(1))
+                 , command=lambda: press(1), highlightbackground='#3E4149')
 button1.grid(row=2, column=0)
 
 button2 = Button(gui, text=' 2 ', fg='black', bg='red', height=1, width=7
-                 , command=lambda: press(2))
+                 , command=lambda: press(2), highlightbackground='#3E4149')
 button2.grid(row=2, column=1)
 
 button3 = Button(gui, text='3', fg='black', bg='red', height=1, width=7
-                 , command=lambda: press(3))
+                 , command=lambda: press(3), highlightbackground='#3E4149')
 button3.grid(row=2, column=2)
 
 button4 = Button(gui, text='4', fg='black', bg='red', height=1, width=7
-                 , command=lambda: press(4))
+                 , command=lambda: press(4), highlightbackground='#3E4149')
 button4.grid(row=3, column=0)
 
 button5 = Button(gui, text='5', fg='black', bg='red', height=1, width=7
-                 , command=lambda: press(5))
+                 , command=lambda: press(5), highlightbackground='#3E4149')
 button5.grid(row=3, column=1)
 
 button6 = Button(gui, text='6', fg='black', bg='red', height=1, width=7
-                 , command=lambda: press(6))
+                 , command=lambda: press(6), highlightbackground='#3E4149')
 button6.grid(row=3, column=2)
 
 button7 = Button(gui, text='7', fg='black', bg='red', height=1, width=7,
-                 command=lambda: press(7))
+                 command=lambda: press(7), highlightbackground='#3E4149')
 button7.grid(row=4, column=0)
 
 button8 = Button(gui, text='8', fg='black', bg='red', height=1, width=7,
-                 command=lambda: press(8))
+                 command=lambda: press(8), highlightbackground='#3E4149')
 button8.grid(row=4, column=1)
 
 button9 = Button(gui, text='9', fg='black', bg='red', height=1, width=7,
-                 command=lambda: press(9))
+                 command=lambda: press(9), highlightbackground='#3E4149')
 button9.grid(row=4, column=2)
 
 button0 = Button(gui, text='0', fg='black', bg='red', height=1, width=7,
-                 command=lambda: press(0))
+                 command=lambda: press(0), highlightbackground='#3E4149')
 button0.grid(row=5, column=0)
 
 plus = Button(gui, text=' + ', fg='black', bg='red', height=1, width=7,
-              command=lambda: press('+'))
+              command=lambda: press('+'), highlightbackground='#3E4149')
 plus.grid(row=2, column=3)
 
 minus = Button(gui, text=' - ', fg='black', bg='red', height=1, width=7,
-               command=lambda: press('-'))
+               command=lambda: press('-'), highlightbackground='#3E4149')
 minus.grid(row=3, column=3)
 
 multiply = Button(gui, text=' * ', fg='black', bg='red', height=1, width=7,
-                  command=lambda: press('*'))
+                  command=lambda: press('*'), highlightbackground='#3E4149')
 multiply.grid(row=4, column=3)
 
 divide = Button(gui, text=' / ', fg='black', bg='red', height=1, width=7,
-                command=lambda: press('/'))
+                command=lambda: press('/'), highlightbackground='#3E4149')
 divide.grid(row=5, column=3)
 
 equal = Button(gui, text=' = ', fg='black', bg='red', height=1, width=7,
-               command=equal_press)
+               command=equal_press, highlightbackground='#3E4149')
 equal.grid(row=5, column=2)
 
 clear = Button(gui, text='Clear', fg='black', bg='red', height=1, width=7,
-               command=clr)
+               command=clr, highlightbackground='#3E4149')
 clear.grid(row=5, column=1)
 
 Decimal = Button(gui, text='.', fg='black', bg='red', height=1, width=7,
-                 command=lambda: press('.'))
+                 command=lambda: press('.'), highlightbackground='#3E4149')
 Decimal.grid(row=6, column=0)
 
 gui.mainloop()
